0058335dbbcd9a95585527210970c4754872e432
LukePNicholas <137431120+LukePNicholas@users.noreply.github.com>
2023-07-19 15:54:19 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,87 @@
+import random
+import word as wordlist
+
+
+
+
+def hangman():
+    word = random.choice(wordlist)
+    guessed = []
+    tries = 6
+    input('Please enter your name: ')
+
+    while tries > 0:
+        for letter in word:
+            if letter in guessed:
+                print(letter)
+            else:
+                print("_")
+
+        guess = input('Enter a letter: ').lower()
+        guessed.append(guess)
+
+        if guess not in word:
+            tries -= 1
+            print("Wrong letter! You have", tries, 'left')
+
+
+
+
+
+    if tries == 0:
+        print("You ran out of guesses! The word was", word)
+
+
+
+
+
+theman = ['''
+       ------    
+       |    |
+       |
+       |
+       |
+       |
+    =======''', '''
+       ------
+       |    | 
+       |    0
+       |
+       |
+       |
+    =======''', '''
+       ------
+       |    |
+       |    0
+       |    |
+       |
+       |
+    =======''', '''
+       ------
+       |    |
+       |    0
+       |   /|
+       |
+       |
+    =======''', '''
+       ------
+       |    |
+       |    0
+       |   /|\
+       |
+       |
+    =======''', '''
+       ------
+       |    |
+       |    0
+       |   /|\
+       |   / 
+       | 
+    =======''', '''
+       ------
+       |    |
+       |    0
+       |   /|\
+       |   / \
+       | 
+    =======''']
