82a6df5a22f80c6b8cb1c20efdbefb54cf3b7e56
LukePNicholas <137431120+LukePNicholas@users.noreply.github.com>
2023-08-15 19:17:30 -0700

    Add files via upload
    My finished project

--- main.py
+++ main.py
@@ -1,16 +1,121 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+with open('Word.py') as f:
+    wordlist = f.readlines()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_random_word():
+    return random.choice(wordlist).lower().strip()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def get_completion(word, guessed):
+    return ''.join([letter if letter in guessed else '_' for letter in word])
+
+
+def is_solved(word, guessed):
+    return len([letter for letter in word if letter not in guessed]) == 0
+
+
+print("Welcome to hangman!")
+input('Please enter your name: ')
+
+
+def hangman():
+    word = get_random_word()
+    guessed = []
+    tries = 6
+    while True:
+        the_man(tries)
+        print(get_completion(word, guessed))
+        guess = input('Enter a letter: ').lower()
+        if len(guess) != 1:
+            print('You must guess exactly one letter')
+            continue
+        if guess in guessed:
+            print("You already guessed that letter!")
+            continue
+        guessed.append(guess)
+        if is_solved(word, guessed):
+            print('Congratulations! You guessed the word', word)
+            return
+        elif guess in word:
+            print("Yes!, guess again")
+        elif guess not in word:
+            tries -= 1
+            print("Wrong letter! You have", tries, 'tries left')
+        if tries == 0:
+            print("You ran out of guesses! The word was", word)
+            the_man(tries)
+            return
+
+
+def the_man(tries):
+    stages = ['''
+       ------    
+       |    |
+       |    0
+       |  / | \\
+       |   / \\
+       |
+    =======''',
+              '''
+       ------
+       |    | 
+       |    0
+       |  / | \\
+       |   /
+       |
+    =======''',
+              '''
+       ------
+       |    |
+       |    0
+       |  / | \\
+       |    
+       |
+    =======''',
+              '''
+       ------
+       |    |
+       |    0
+       |  / | 
+       |
+       |
+    =======''',
+              '''
+       ------
+       |    |
+       |    0
+       |    |
+       |
+       |
+    =======''',
+              '''
+       ------
+       |    |
+       |    0
+       |   
+       |   
+       | 
+    =======''',
+              '''
+       ------
+       |    |
+       |
+       |
+       | 
+       | 
+    =======''']
+    print(stages[tries])
+
+
+play_again = True
+while play_again:
+    hangman()
+    response = ""
+    while response != 'y' and response != 'n':
+        response = input('Play again? Y/N ').lower()
+        if response == 'n':
+            play_again = False
+        elif response != 'y':
+            print("Please choose y or n")
