b42f03eb07608817089902b60a63a34462944111
LukePNicholas <137431120+LukePNicholas@users.noreply.github.com>
2023-08-30 19:16:47 -0700

    Improved main program

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ with open('Word.py') as f:
 
 
 def get_random_word():
-    """Returns a random word from the word list."""
+    """Chooses a random word from the word list and returns it."""
     return random.choice(wordlist).lower().strip()
 
 
@@ -20,10 +20,13 @@ def is_solved(word, guessed):
 
 
 name = input('Please enter your name: ')
-print("Welcome to hangman", name, '!')
+print(f"Welcome to Hangman, {name}!")
 
 
 def hangman():
+    """Gives user six tries to guess letters in the random word. If the letter they guess is incorrect,
+     it subtracts a try, shows the next stage of the_man, and prompts them to input another letter until
+     they either guess the word correctly or run out of tries."""
     word = get_random_word()
     guessed = []
     tries = 6
@@ -31,29 +34,33 @@ def hangman():
         the_man(tries)
         print(get_completion(word, guessed))
         guess = input('Enter a letter: ').lower()
+        if not guess.isalpha():
+            print('You must enter a valid character.')
+            continue
         if len(guess) != 1:
-            print('You must guess exactly one letter')
+            print('You must guess exactly one letter.')
             continue
         if guess in guessed:
             print("You already guessed that letter!")
             continue
         guessed.append(guess)
         if is_solved(word, guessed):
-            print('Congratulations!', name, "You guessed the word", word)
+            print(f'Congratulations, {name}! You guessed the word {word}.')
             return
         elif guess in word:
-            print("Yes!, guess again")
+            print("Yes! Guess again.")
         elif guess not in word:
             tries -= 1
-            print("Wrong letter! You have", tries, 'tries left')
+            print("Wrong letter! You have", tries, 'tries left.')
         if tries == 0:
-            print("You ran out of guesses! The word was", word)
+            print(f"You ran out of guesses! The word was {word}.")
             the_man(tries)
             return
 
 
 def the_man(tries):
-    """The different stages of the hangman graphic."""
+    """Prints the current step of the hangman, and prints the next step of the hangman
+    when a guess is incorrect."""
     stages = ['''
        ------    
        |    |
@@ -118,8 +125,9 @@ while play_again:
     hangman()
     response = ""
     while response != 'y' and response != 'n':
-        response = input('Would you like to Play again? Y/N ').lower()
+        response = input('Would you like to play again? Y/N ').lower()
         if response == 'n':
+            print('Thank you for playing!')
             play_again = False
         elif response != 'y':
             print("Please choose y or n")
